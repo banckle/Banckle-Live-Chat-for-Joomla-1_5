f6c4e7bf22b19d8e3b5a0fd47e02ab13ec4ac803
Assad Mahmood Qazi <assadvirgo@gmail.com>
2013-07-25 12:56:30 +0500

    Initial Commit

---
+++ tmpl/default.php
@@ -0,0 +1,22 @@
+<?php
+/*------------------------------------------------------------------------
+# mod_bancklelivechat - Banckle Live Chat for Joomla
+# ------------------------------------------------------------------------
+# author    Imran Anwar <imran.anwar@banckle.com>
+# copyright Copyright (C) 2011 banckle.com. All Rights Reserved.
+# @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+# Websites: http://banckle.com
+# Technical Support:  Docs - http://banckle.com/wiki/display/livechat/integration-with-joomla.html
+-------------------------------------------------------------------------*/
+// no direct access
+defined('_JEXEC') or die('Restricted access'); ?>
+
+<style>
+#banckleLiveChatButton img{width:150px;}
+</style>
+<div id=banckleLiveChatButton>
+<?php
+    echo $params->get("banckleLCWidget");
+
+?>
+</div>
